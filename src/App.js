63d98e7c157d3f2b5de6ea9c169865c4fe56f82f
Karthik-M-95 <karthikappu1995@gmail.com>
2024-05-19 21:51:45 +0530

    MyTracker updating App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
+import NavigatorPreLogin from "./component/Navigator";
 
 
 function App() {
   return (
     <div className="App">
       
-      <header className="App-header">
-        {/* <img src='https://krishnastore.in/images/cache/545-743x1000.jpg' className="App-logo" alt="logo" /> */}
-        <p>
-          This is React initial deployment
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* <NavigatorPreLogin /> */}
+    <p style={{alignContent:'center', display:'flex', justifyContent:'center'}}>Hello this is demo web still under developement</p>
+
     </div>
   );
 }
