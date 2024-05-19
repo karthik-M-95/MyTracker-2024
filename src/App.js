7fc75918e1baab772a9389babca6bccf8d20dc04
Karthik-M-95 <karthikappu1995@gmail.com>
2024-05-19 21:21:23 +0530

    React MyTracker first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        {/* <img src='https://krishnastore.in/images/cache/545-743x1000.jpg' className="App-logo" alt="logo" /> */}
+        <p>
+          This is React initial deployment
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
